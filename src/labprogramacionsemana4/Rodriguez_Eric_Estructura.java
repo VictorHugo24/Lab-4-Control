674bcdf6b36f83642db9a6a9d22c4bcfeef0c215
Eric Rodriguez <Eric Rodriguez@ERIC-RODRIGUEZ>
2025-05-16 10:29:52 -0600

    Avance

--- src/labprogramacionsemana4/Rodriguez_Eric_Estructura.java
+++ src/labprogramacionsemana4/Rodriguez_Eric_Estructura.java
@@ -10,43 +10,57 @@ import java.util.Scanner;
  */
 public class Rodriguez_Eric_Estructura {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner (System.in);
-        int opcion;
-        int numcesar = 0;
-        int cifradocesar, cifradocesarr;
-        String frase = "";
+        String fraseog ;
+        int desplazamiento;
+        sc.useDelimiter("\n");
+        int opcion = 0;
+        while (opcion != 4){
         System.out.println("*** MENU ***");
         System.out.println("1. Cifrado");
         System.out.println("2. Filtrar");
         System.out.println("3. Codigo Enigma");
+        System.out.println("4. Salir");
         opcion = sc.nextInt();
         switch (opcion){
             case 1:
-                System.out.println("Favor ingresar una frase: ");
-                frase = sc.nextLine();
-                while (numcesar <= 0){
-                System.out.println("Favor ingrese un numero positivo: ");
-                numcesar = sc.nextInt();
-                } 
-                if (numcesar <= 0){
-                    System.out.println("Favor ingrese un numero positivo");
-                
+                //Codigo cesar
+                 System.out.println("Ingrese la frase a cifrar:");
+        fraseog = sc.next();
+        
+                System.out.println("Ingrese el numero de posiciones a desplazar:");
+         desplazamiento = sc.nextInt();
+        
+        String fraseCifrada = "";
+        int longitud = fraseog.length();
+        int i = 0;
+
+        while (i < longitud) {
+            char caracterog = fraseog.charAt(i);
+            char caracterCifrado;
+
+            if (caracterog >= 'a' && caracterog <= 'z') {
+                int posicionog = caracterog - 'a';
+                int posicioncr = (posicionog + desplazamiento) % 26;
+                caracterCifrado = (char) ('a' + posicioncr);
+            } else if (caracterog >= 'A' && caracterog <= 'Z') {
+                int posicionOriginal = caracterog - 'A';
+                int posicioncr = (posicionOriginal + desplazamiento) % 26;
+                caracterCifrado = (char) ('A' + posicioncr);
+            } else {
+                caracterCifrado = caracterog;
             }
-                for (int i = 0; i < frase.length(); i++) {
-                    char caracter = frase.charAt(i);
-                    if (caracter >= 'A' && caracter <= 'Z' || caracter >= 'a' && caracter <= 'z'){
-                        cifradocesarr = (int)caracter;
-                        cifradocesar = cifradocesarr + numcesar;
-                        while (cifradocesar > (int)'Z'){
-                            cifradocesar -= 26;
-                        }
-                        while (cifradocesar < (int) 'A'){
-                            cifradocesar += 26;
-                        }
-                    
-                }
-                    
-                }
+
+            fraseCifrada = fraseCifrada + caracterCifrado;
+            i = i + 1;
+        }
+
+        System.out.println("Su frase cifrada es: " + fraseCifrada);
+         
+    
+                break;
+                //Filtrar
             case 2:
                 sc.useDelimiter("\n"); 
         
@@ -65,8 +79,8 @@ public class Rodriguez_Eric_Estructura {
         int cont = 0;
         
         // Ciclo que recorre la frase
-        while (cont < frase.length()) {
-            char c = frase.charAt(cont); // Obtener el carácter actual
+        while (cont < frase2.length()) {
+            char c = frase2.charAt(cont); // Obtener el carácter actual
 
             // Si el caracter es un espacio, significa que hemos encontrado una palabra
             if (c == ' ') {
@@ -84,14 +98,16 @@ public class Rodriguez_Eric_Estructura {
         // Verificamos la ultima palabra (despues del ultimo espacio)
         if (palabra.length() >= cantidad) {  // Mayor o igual longitud para la ultima palabra
             System.out.println(palabra); // Imprimir la última palabra si cumple con la longitud
-        }
+        } break;
          default:
                 System.out.println("Opcion no valida, intente de nuevo");
+                break;
     }
 
            
-            
-        }
+        
+        }  
     }
+}
     
 
